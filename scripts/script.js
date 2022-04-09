bb398c07268fe90aaff574af175ba207e9b19fa9
Kira Gorinchoi <voronova.kira@gmail.com>
2022-04-09 10:43:16 +0300

    fix: правки по ревью

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// переменные формы
+const popup = document.querySelector('.popup');
+const formElement = popup.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
+const closeFormButton = popup.querySelector('.button_type_popup-close');
+
+// переменные профиля
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const openFormButton = document.querySelector('.button_type_profile-edit');
+
+function openForm() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
+}
+
+function closeForm() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+
+    closeForm();
+}
+
+openFormButton.addEventListener('click', openForm);
+closeFormButton.addEventListener('click', closeForm);
+formElement.addEventListener('submit', formSubmitHandler);
